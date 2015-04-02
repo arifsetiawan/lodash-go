6b3a61d386c2bef91d2a7fcf917d3d0e7ca1d192
mparaiso <aikah@free.fr>
2015-04-02 12:09:47 +0200

    doc

---
+++ doc.go
@@ -0,0 +1,24 @@
+// Copyright 2015 mparaiso<mparaiso@online.fr>. All rights reserved.
+// Use of this source code is governed by a MIT-style
+// license that can be found in the LICENSE file.
+
+/*
+lo provides functional utilities to Go similar to lodash.js and underscore.js
+
+The following functions are implemented :
+
+Arrays:
+
+    - Chunk
+    - Difference
+    - Union
+    - Intersection
+    - Xor
+    - Without
+    - Unique
+    - Equal
+    - Zip
+    - Compact
+    - First
+    - Last
+*/
--- lo.go
+++ lo.go
@@ -2,7 +2,6 @@
 // Use of this source code is governed by a MIT-style
 // license that can be found in the LICENSE file.
 
-// lo provides functional utilities for Go,similar to lodash.js and underscore.js
 package lo
 
 import (
