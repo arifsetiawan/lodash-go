e2463d0e41952a64f6849857896a8c8037bb8274
mparaiso <aikah@free.fr>
2016-06-23 14:52:09 +0200

    added some tests

--- lo_test.go
+++ lo_test.go
@@ -10,268 +10,292 @@ import (
 	"github.com/mparaiso/lodash-go"
 )
 
-func TestChunk(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		collection []interface{}
-		length     int
-		// Expected results.
-		want []interface{}
-	}{
-		{"first test", []interface{}{1, 2, 3, 4}, 2, []interface{}{[]interface{}{1, 2}, []interface{}{3, 4}}},
+//func TestChunk(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		collection []interface{}
+//		length     int
+//		// Expected results.
+//		want []interface{}
+//	}{
+//		{"first test", []interface{}{1, 2, 3, 4}, 2, []interface{}{[]interface{}{1, 2}, []interface{}{3, 4}}},
+//	}
+//	for _, tt := range tests {
+//		if got := lo.Chunk(tt.collection, tt.length); !reflect.DeepEqual(got, tt.want) {
+//			t.Errorf("%q. Chunk() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
+
+//func TestDifference(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		array  []interface{}
+//		values []interface{}
+//		// Expected results.
+//		want []interface{}
+//	}{
+//		{"Difference between 2 arrays", []interface{}{1, 2, 3, 4}, []interface{}{2, 4}, []interface{}{1, 3}},
+//	}
+//	for _, tt := range tests {
+//		if got := lo.Difference(tt.array, tt.values); !reflect.DeepEqual(got, tt.want) {
+//			t.Errorf("%q. Difference() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
+
+//func TestWithout(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		array  []interface{}
+//		values []interface{}
+//		// Expected results.
+//		want []interface{}
+//	}{
+//		{"Array 1 without Array 2", []interface{}{1, 2, 3, 4}, []interface{}{2, 4}, []interface{}{1, 3}},
+//	}
+//	for _, tt := range tests {
+//		if got := lo.Without(tt.array, tt.values...); !reflect.DeepEqual(got, tt.want) {
+//			t.Errorf("%q. Without() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
+
+//func TestIntersection(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		arrays [][]interface{}
+//		// Expected results.
+//		want []interface{}
+//	}{
+//		{
+//			"Intersection of arrays",
+//			[][]interface{}{{1, 2, 3, 4}, {2, 3, 5}, {0, 2, 3, 6}},
+//			[]interface{}{2, 3},
+//		},
+//	}
+//	for _, tt := range tests {
+//		if got := lo.Intersection(tt.arrays...); !reflect.DeepEqual(got, tt.want) {
+//			t.Errorf("%q. Intersection() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
+
+//func TestXor(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		arrays [][]interface{}
+//		// Expected results.
+//		want []interface{}
+//	}{
+//		{
+//			"Xor of arrays",
+//			[][]interface{}{{1, 2, 3, 4}, {2, 3, 5}, {0, 2, 3, 6}},
+//			[]interface{}{1, 4, 5, 0, 2, 3, 6},
+//		},
+//	}
+//	for _, tt := range tests {
+//		if got := lo.Xor(tt.arrays...); !reflect.DeepEqual(got, tt.want) {
+//			t.Errorf("%q. Xor() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
+
+//func TestLastIndexOf(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		array     []interface{}
+//		value     interface{}
+//		fromIndex int
+//		// Expected results.
+//		want int
+//	}{
+//		{"Last index", []interface{}{1, 2, 3, 4, 5, 2}, 2, 0, 5},
+//	}
+//	for _, tt := range tests {
+//		if got := lo.LastIndexOf(tt.array, tt.value, tt.fromIndex); got != tt.want {
+//			t.Errorf("%q. LastIndexOf() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
+
+//func TestUnion(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		arrays [][]interface{}
+//		// Expected results.
+//		want []interface{}
+//	}{}
+//	for _, tt := range tests {
+//		if got := lo.Union(tt.arrays...); !reflect.DeepEqual(got, tt.want) {
+//			t.Errorf("%q. Union() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
+
+//func TestUnique(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		array []interface{}
+//		// Expected results.
+//		want []interface{}
+//	}{
+//		{"Unique values", []interface{}{6, 4, 5, 6, 2, 4}, []interface{}{6, 4, 5, 2}},
+//	}
+//	for _, tt := range tests {
+//		if got := lo.Unique(tt.array); !reflect.DeepEqual(got, tt.want) {
+//			t.Errorf("%q. Unique() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
+
+func ExamplePipeline_Map() {
+	// Compute the sum of all countries people
+	type Country struct {
+		Name       string
+		Population int
 	}
-	for _, tt := range tests {
-		if got := lo.Chunk(tt.collection, tt.length); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Chunk() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
+	var total int
+	err := lo.In([]Country{{"France", 1000}, {"Spain", 5000}}).
+		Map(func(country Country) int { return country.Population }).
+		Reduce(func(total int, count int) int { return total + count }, 0).
+		Out(&total)
+	fmt.Println(err)
+	fmt.Println(total)
+	// Output:
+	// <nil>
+	// 6000
 }
 
-func TestDifference(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		array  []interface{}
-		values []interface{}
-		// Expected results.
-		want []interface{}
-	}{
-		{"Difference between 2 arrays", []interface{}{1, 2, 3, 4}, []interface{}{2, 4}, []interface{}{1, 3}},
+func ExamplePipeline_Reduce() {
+	// Group people by country
+	// Demonstrates the use of Reduce to transform an collection
+	// into a map
+	type Person struct {
+		Name    string
+		Country string
+		Age     int
 	}
-	for _, tt := range tests {
-		if got := lo.Difference(tt.array, tt.values); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Difference() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
+
+	var adultPeopleByCountry map[string]int
+	err := lo.In([]Person{
+		{"John", "France", 18},
+		{"Jane", "England", 16},
+		{"Jack", "France", 20},
+		{"Anna", "Spain", 19},
+		{"Eduardo", "Spain", 30},
+		{"Michel", "France", 12}}).
+		Filter(func(person Person) bool { return person.Age >= 18 }).
+		Reduce(func(list map[string]int, person Person) map[string]int {
+		list[person.Country] = list[person.Country] + 1
+		return list
+	}, map[string]int{}).
+		Out(&adultPeopleByCountry)
+
+	fmt.Println(err)
+	fmt.Println(adultPeopleByCountry["France"])
+	fmt.Println(adultPeopleByCountry["Spain"])
+	fmt.Println(adultPeopleByCountry["England"])
+	// Output:
+	// <nil>
+	// 2
+	// 2
+	// 0
+
 }
 
-func TestWithout(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		array  []interface{}
-		values []interface{}
-		// Expected results.
-		want []interface{}
-	}{
-		{"Array 1 without Array 2", []interface{}{1, 2, 3, 4}, []interface{}{2, 4}, []interface{}{1, 3}},
-	}
-	for _, tt := range tests {
-		if got := lo.Without(tt.array, tt.values...); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Without() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
+//func TestLast(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		array []interface{}
+//		// Expected results.
+//		want interface{}
+//	}{
+//		{"Last element of collection", []interface{}{3, 4}, 4},
+//	}
+//	for _, tt := range tests {
+//		if got := lo.Last(tt.array); !reflect.DeepEqual(got, tt.want) {
+//			t.Errorf("%q. Last() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
 
-func TestIntersection(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		arrays [][]interface{}
-		// Expected results.
-		want []interface{}
-	}{
-		{
-			"Intersection of arrays",
-			[][]interface{}{{1, 2, 3, 4}, {2, 3, 5}, {0, 2, 3, 6}},
-			[]interface{}{2, 3},
-		},
-	}
-	for _, tt := range tests {
-		if got := lo.Intersection(tt.arrays...); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Intersection() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
+//func TestZip(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		arrays [][]interface{}
+//		// Expected results.
+//		want []interface{}
+//	}{
+//	// TODO: Add test cases.
+//	}
+//	for _, tt := range tests {
+//		if got := lo.Zip(tt.arrays...); !reflect.DeepEqual(got, tt.want) {
+//			t.Errorf("%q. Zip() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
 
-func TestXor(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		arrays [][]interface{}
-		// Expected results.
-		want []interface{}
-	}{
-		{
-			"Xor of arrays",
-			[][]interface{}{{1, 2, 3, 4}, {2, 3, 5}, {0, 2, 3, 6}},
-			[]interface{}{1, 4, 5, 0, 2, 3, 6},
-		},
-	}
-	for _, tt := range tests {
-		if got := lo.Xor(tt.arrays...); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Xor() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
-
-func TestLastIndexOf(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		array     []interface{}
-		value     interface{}
-		fromIndex int
-		// Expected results.
-		want int
-	}{
-		{"Last index", []interface{}{1, 2, 3, 4, 5, 2}, 2, 0, 5},
-	}
-	for _, tt := range tests {
-		if got := lo.LastIndexOf(tt.array, tt.value, tt.fromIndex); got != tt.want {
-			t.Errorf("%q. LastIndexOf() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
-
-func TestUnion(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		arrays [][]interface{}
-		// Expected results.
-		want []interface{}
-	}{}
-	for _, tt := range tests {
-		if got := lo.Union(tt.arrays...); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Union() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
-
-func TestUnique(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		array []interface{}
-		// Expected results.
-		want []interface{}
-	}{
-		{"Unique values", []interface{}{6, 4, 5, 6, 2, 4}, []interface{}{6, 4, 5, 2}},
-	}
-	for _, tt := range tests {
-		if got := lo.Unique(tt.array); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Unique() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
-
-func TestFirst(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		array []interface{}
-		// Expected results.
-		want interface{}
-	}{
-		{"First element of collection", []interface{}{3, 4}, 3},
-	}
-	for _, tt := range tests {
-		if got := lo.First(tt.array); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. First() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
-
-func TestLast(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		array []interface{}
-		// Expected results.
-		want interface{}
-	}{
-		{"Last element of collection", []interface{}{3, 4}, 4},
-	}
-	for _, tt := range tests {
-		if got := lo.Last(tt.array); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Last() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
-
-func TestZip(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		arrays [][]interface{}
-		// Expected results.
-		want []interface{}
-	}{
-	// TODO: Add test cases.
-	}
-	for _, tt := range tests {
-		if got := lo.Zip(tt.arrays...); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Zip() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
-
-func TestEqual(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		arrays [][]interface{}
-		// Expected results.
-		want bool
-	}{
-		{"Array are equals", [][]interface{}{{1, 2}, {1, 2}, {1, 2}}, true},
-	}
-	for _, tt := range tests {
-		if got := lo.Equal(tt.arrays...); got != tt.want {
-			t.Errorf("%q. Equal() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
-
-func TestCompact(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		array []interface{}
-		// Expected results.
-		want []interface{}
-	}{
-		{"Remove nil values", []interface{}{0, nil, 2, nil}, []interface{}{0, 2}},
-	}
-	for _, tt := range tests {
-		if got := lo.Compact(tt.array); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Compact() = %v, want %v", tt.name, got, tt.want)
-		}
-	}
-}
+//func TestEqual(t *testing.T) {
+//	tests := []struct {
+//		// Test description.
+//		name string
+//		// Parameters.
+//		arrays [][]interface{}
+//		// Expected results.
+//		want bool
+//	}{
+//		{"Array are equals", [][]interface{}{{1, 2}, {1, 2}, {1, 2}}, true},
+//	}
+//	for _, tt := range tests {
+//		if got := lo.Equal(tt.arrays...); got != tt.want {
+//			t.Errorf("%q. Equal() = %v, want %v", tt.name, got, tt.want)
+//		}
+//	}
+//}
 
 func TestFilter(t *testing.T) {
 	tests := []struct {
 		// Test description.
 		name string
 		// Parameters.
-		collection []interface{}
-		predicate  func(interface{}, int, []interface{}) bool
+		collection interface{}
+		predicate  interface{}
 		// Expected results.
-		want []interface{}
+		want interface{}
 	}{
-		{"Filter even numbers", []interface{}{1, 2, 3, 4}, func(element interface{}, index int, collection []interface{}) bool {
-			return element.(int)%2 == 0
-		}, []interface{}{2, 4}},
+		{"Filter even numbers", []int{1, 2, 3, 4}, func(element int, index int, collection []int) bool {
+			return element%2 == 0
+		}, []int{2, 4}},
 	}
+	var got interface{}
 	for _, tt := range tests {
-		if got := lo.Filter(tt.collection, tt.predicate); !reflect.DeepEqual(got, tt.want) {
-			t.Errorf("%q. Filter() = %v, want %v", tt.name, got, tt.want)
+
+		if err := lo.Filter(tt.collection, tt.predicate, &got); err != nil {
+			t.Fatalf("Error Should be nil, got", err)
+		}
+		if !reflect.DeepEqual(tt.want, got) {
+			t.Errorf("Got '%v', want '%v'", got, tt.want)
 		}
 	}
 }
@@ -281,34 +305,62 @@ func TestIndexOf(t *testing.T) {
 		// Test description.
 		name string
 		// Parameters.
-		collection []interface{}
+		collection interface{}
 		element    interface{}
 		index      int
 		// Expected results.
 		want int
 	}{
-		{"", []interface{}{1, 2, 3}, 3, 0, 2},
+		{"", []int{1, 2, 3}, 3, 0, 2},
 	}
 	for _, tt := range tests {
-		if got := lo.IndexOf(tt.collection, tt.element, tt.index); got != tt.want {
-			t.Errorf("%q. IndexOf() = %v, want %v", tt.name, got, tt.want)
+		if got, err := lo.IndexOf(tt.collection, tt.element, tt.index); err != nil {
+			t.Fatal(err)
+		} else if got != tt.want {
+			t.Errorf("lo.IndexOf : got '%d', want '%d'", got, tt.want)
 		}
 	}
 }
+func ExampleDifference() {
+	var difference []string
+	err := lo.Difference([]string{"a", "b", "c", "d"}, []string{"a", "c"}, &difference)
+	fmt.Println(err)
+	fmt.Println(difference)
+	// Output:
+	// <nil>
+	// [b d]
+}
 
-func TestForEach(t *testing.T) {
-	tests := []struct {
-		// Test description.
-		name string
-		// Parameters.
-		collection []interface{}
-		handler    func(interface{}, int, []interface{})
-	}{
-		{"", []interface{}{1, 2, 3, 4}, func(element interface{}, i int, collection []interface{}) { _ = element.(int) }},
-	}
-	for _, tt := range tests {
-		lo.ForEach(tt.collection, tt.handler)
-	}
+func ExampleIntersection() {
+	var intersection []int
+	err := lo.Intersection([]int{1, 2, 3, 4}, []int{0, 2, 4, 5}, &intersection)
+	fmt.Println(err)
+	fmt.Println(intersection)
+	// Ouput:
+	// <nil>
+	// [2 4]
+}
+
+func ExampleUnique() {
+	var uniqueValues []string
+	err := lo.Unique([]string{"a", "e", "a", "c", "b", "d"}, &uniqueValues)
+	fmt.Println(err)
+	fmt.Println(uniqueValues)
+	// Output:
+	// <nil>
+	// [a e c b d]
+}
+
+func ExampleFilter() {
+	var evenNumbers []int
+	err := lo.Filter([]int{0, 1, 2, 3, 4}, func(element int) bool {
+		return element%2 == 0
+	}, &evenNumbers)
+	fmt.Println(err)
+	fmt.Println(evenNumbers)
+	// Output:
+	// <nil>
+	// [0 2 4]
 }
 
 func TestReduce(t *testing.T) {
@@ -346,9 +398,9 @@ func TestReduce(t *testing.T) {
 	var illegalParameterType = reflect.TypeOf(lo.IncorrectInputParameterType(""))
 	var incorrectOutputParameterType = reflect.TypeOf(lo.IncorrectOutputParameterType(""))
 	for _, test := range []Args{
-		{"", func() {}, "", reflect.TypeOf(lo.NotASlice("")), nil, "NotASliceError"},
+		{"", func() {}, "", reflect.TypeOf(lo.NotACollection("")), nil, "NotASliceError"},
 		{[8]byte{}, 1, "", reflect.TypeOf(lo.NotAFunction("")), nil, "NotAFunctionError"},
-		{[]int{}, func(result int, element int) int { return result }, 0, reflect.TypeOf(lo.NotAPointer("")), 0, "NotAPointer"},
+		{[]int{}, func(result int, element int) int { return result }, 0, reflect.TypeOf(lo.NotPointer("")), 0, "NotAPointer"},
 		{[]int{1, 2, 3}, func(result int, element int) int { return result }, 0, reflect.TypeOf(lo.NotAssignable("")), &struct{}{}, "NotAssignableError"},
 		{[]int{1, 2, 3}, func(result string, element string) string { return result }, 0, illegalParameterType, nil, "IllegalParameterType first"},
 		{[]int{1, 2, 3}, func(result int, element string) int { return result }, 0, illegalParameterType, nil, "IllegalParameterType second"},
@@ -360,7 +412,7 @@ func TestReduce(t *testing.T) {
 		t.Logf("\t%s", test.Label)
 
 		err := lo.Reduce(test.Collection, test.Function, test.Initial, test.Result)
-		t.Log(test.Result, err)
+		//t.Log(test.Result, err)
 		if err == nil {
 			t.Fatalf("\t\tError %#v should not be nil", err)
 		}
@@ -395,4 +447,47 @@ func ExampleReduce_second() {
 	lo.Reduce([]int{1, 2, 3, 4}, func(result int, element int) int { return result + element }, 0, &sum)
 	fmt.Print(sum)
 	// Output: 10
+}
+
+func ExampleMap() {
+	var result []int
+	err := lo.Map([]int{2, 3, 4}, func(element int) int { return element * 2 }, &result)
+	fmt.Println(err)
+	fmt.Println(result)
+
+	// Output:
+	// <nil>
+	// [4 6 8]
+
+}
+
+func ExampleMap_second() {
+	type Data struct {
+		Name  string
+		Index int
+	}
+	var result2 []Data
+	err := lo.Map([]string{"John", "Jane", "Jack"}, func(element string, index int) Data {
+		return Data{element, index}
+	}, &result2)
+	fmt.Println(err)
+	fmt.Println(result2)
+	// Output:
+	// <nil>
+	// [{John 0} {Jane 1} {Jack 2}]
+}
+
+func ExampleMap_third() {
+	// The function mapper can return an error as a second argument,
+	// which will be returned by lo.Map
+	var result []int
+	err := lo.Map([]bool{true, false, true}, func(element bool, index int, collection []bool) (int, error) {
+		if !element {
+			return 0, fmt.Errorf("Elements shouldn't be false, got '%v' at index '%d' for collection '%v'", element, index, collection)
+		}
+		return index, nil
+	}, &result)
+	fmt.Println(err)
+	// Output:
+	// Elements shouldn't be false, got 'false' at index '1' for collection '[true false true]'
 }
